e94ede624d832ae57b9e55def8b8f76a23ecf0db
Jenalee Palmer <jenaleepalmer@gmail.com>
2021-12-16 12:47:12 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function removeAddDefinitionButton() {
+    document.getElementById("addDefinitionButton").remove();
+}
+
+function loginButtonChange() {
+    document.getElementById("loginButton").innerText = "Logout";
+}
+
+function likeNinja() {
+    alert("Ninja was liked");
+}
